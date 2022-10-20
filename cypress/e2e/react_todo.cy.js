3b8cdc348518995089a1f07d70cb2850532d072c
Arham <tufayel842@gmail.com>
2022-10-20 21:15:02 +0600

    first commit

---
+++ cypress/e2e/react_todo.cy.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+/// <reference types="Cypress"/>
+
+
+describe('App Component', () => {
+    beforeEach(() => {
+        // eslint-disable-next-line no-undef
+        cy.visit('http://localhost:3000')
+        Cypress.on('uncaught:exception', (err, runnable) => { return false; })
+    })
+
+    it('Verify URL and TITLE', () => {
+        cy.url().should('include', 'localhost')
+        cy.url().should('eq', 'http://localhost:3000/')
+
+        cy.title().should('include', 'React')
+        cy.title().should('eq', 'React App-2')
+    })
+
+    it('Verify input and button', () => {
+        // eslint-disable-next-line no-undef
+        cy.get('input').type('todo-1')
+        cy.get("[type='submit']").click()
+        cy.get('p').should('have.text', 'todo-1')
+        cy.get("input[type='checkbox']").check().should('be.checked')
+        cy.get("input[type='checkbox']").uncheck()
+        cy.get('#edit').click()
+        cy.get("[type='submit']").click()
+        cy.get('#dlt').click()
+    })
+
+    it('Verify pendingBar and clearItems', () => {
+        cy.get('input').type('todo-1')
+        cy.get("[type='submit']").click()
+        cy.get("input[type='checkbox']").check().should('be.checked')
+        cy.get('.message').should('have.text', 'You have no pending task')
+        cy.get('#remove').click()
+    })
+
+})
---
+++ src/components/Clear_todo.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react'
+
+export default function Clear_todo({ clearTodos, pending, message, removeMessage, todos }) {
+    
+    const _pending = () => {
+        if(pending === 1 ) {
+            return 'a'
+        } else if(pending < 1) {
+            return 'no'
+        }
+        return pending
+    }
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            removeMessage()
+        }, 3000)
+        return () => clearTimeout(timeout)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [todos])
+
+    return (
+        <div className='clear-todo'>
+            <div className={ message.active ? `${message.showMessage} message` : 'message' }>
+                You have { _pending() } pending { pending <= 1 ? 'task' : 'tasks' }
+            </div>
+            <button onClick={ clearTodos } id='remove'>clear all</button>
+        </div>
+    )
+}
